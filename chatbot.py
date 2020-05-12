b4bfcd4487e133fd8d858dc58b6910d47e57d1b4
sharnoor404 <sharnoor404@gmail.com>
2020-05-12 18:18:14 +0530

    essential libraries

---
+++ chatbot.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 21:09:56 202
+
+@author: sharn_6qt6gtn
+"""
+
+#creating a chatbot that answers user queries
+
+#STEP 1 : IMPORTING REQUIRED LIBRARIES
+
+import nltk
+import io
+#library that deals with opening and reading a file
+
+import numpy as np
+#library to perform mathematical operations
+
+import random
+#used for random selection of words from text
+
+import string 
+#this library will be used to process standard python strings
+import warnings
+warnings.filterwarnings('ignore')
+#this would ignore any earning that is incurred while running our application
+
