0970ab8e49a1f5146c8069fa87768737f071104d
sharnoor404 <sharnoor404@gmail.com>
2020-05-12 22:54:59 +0530

    code

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,8 @@
 """
 Created on Wed Mar 18 21:09:56 202
 
-@author: sharn_6qt6gtn
+@author: harnoor
 """
-
-#creating a chatbot that answers user queries
-
-#STEP 1 : IMPORTING REQUIRED LIBRARIES
-
 import nltk
 import io
 #library that deals with opening and reading a file
@@ -25,3 +20,19 @@ import warnings
 warnings.filterwarnings('ignore')
 #this would ignore any earning that is incurred while running our application
 
+#STEP 2 :
+
+file=open('D:\chatbot\globalWarming.txt','r',errors='ignore') 
+#setting target to our file
+data=file.read()
+#allows to read data from our target file
+data=data.lower()
+#converts data to lower
+nltk.download('punkt')
+#used for sentence tokenization i.e it converts a string to a list of words
+nltk.download('wordnet')
+#lexical database for english language, nltk refers to it during run
+sentenceTokens=nltk.sent_tokenize(data)
+#converting text into sentences: creating sentence tokens
+wordTokens=nltk.word_tokenize(data)
+#converting the sentences to words: creating word tokens
